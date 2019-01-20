b87ee4ea96a368f592ec847c1f98f4585543c333
javaComSci <iramanat@purdue.edu>
2019-01-20 00:01:09 -0500

    Change water goals

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      overallGoal: '',
-      waterLeft: 0,
+      overallGoal: 8,
+      waterLeft: 8,
     };
   }
 
@@ -16,13 +16,21 @@ export default class App extends React.Component {
     });
   }
 
+  setGoal = (overallGoal) => {
+    this.setState({
+      overallGoal: parseInt(overallGoal),
+      waterLeft: parseInt(overallGoal)
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{color: '#ff6666', fontWeight:'bold', fontSize: 40, fontFamily: 'Papyrus'}}> Water </Text>
-        <TextInput style={{height: 30}} placeholder="Goal water amount" onChangeText={(overallGoal) => this.setState({overallGoal})} />
-        <Text> Water Left for Today: {this.state.waterLeft} </Text>
-        <Button onPress={this.increaseWaterConsumedCount} title="+ water consumed"/>
+        <Text style={{color: '#0033cc', fontWeight:'bold', fontSize: 40, fontFamily: 'Papyrus'}}> Water </Text>
+        <TextInput style = {{fontSize: 20}} placeholder="Goal water amount" onChangeText={this.setGoal} />
+        <Text style = {{fontSize: 20}}> Your goal for today is: {this.state.overallGoal} </Text>
+        <Text style = {{fontSize: 20}}> {this.state.waterLeft != 0 ? "Water left: " + this.state.waterLeft : "You have finished"} </Text>
+        <Button color='#0033cc' onPress={this.increaseWaterConsumedCount} title="+ water consumed"/>
       </View>
     );
   }
@@ -30,8 +38,8 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 4,
-    backgroundColor: '#FFF1EE',
+    flex: 1,
+    backgroundColor: '#ccd9ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
